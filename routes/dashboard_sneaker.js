2f27c2932876fa1de63ec5759c9d864c023240d5
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-24 19:48:08 +0200

    done tags

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -2,9 +2,10 @@ const express = require("express"); // import express in this module
 const router = express.Router(); // create an app sub-module (router)
 const Sneaker = require("../models/Sneaker");
 const uploader = require("../config/cloudinary");
+const Tag = require("../models/Tag");
 
-router.get("/create", (req, res, next) => {
-  res.render("products_add");
+router.get("/create",async (req, res, next) => {
+  res.render("products_add", { tags: await Tag.find() });
 });
 
 // C
@@ -33,6 +34,7 @@ router.get("/", async (req, res) => {
 router.get("/product-edit/:id", async (req, res, next) => {
   res.render("product_edit", {
     sneaker: await Sneaker.findById(req.params.id),
+    tags: await Tag.find(),
   });
 });
 
@@ -55,6 +57,16 @@ router.get("/product-delete/:id", async (req, res, next) => {
     res.redirect("/dashboard");
   } catch (err) {
     next(err);
+  }
+});
+
+// TAG 
+router.post("/tag-add", async (req, res, next) => {
+  try {
+    await Tag.create(req.body);
+    res.redirect("/dashboard/create");
+  } catch (error) {
+    next(error); 
   }
 });
 

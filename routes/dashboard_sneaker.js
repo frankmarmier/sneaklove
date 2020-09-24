c0d4b80a18ae715ec5360a9fa30aa58c261931f7
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-24 17:08:35 +0200

    problème edit

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -31,15 +31,35 @@ router.post("/prod-add", async (req, res, next) => {
 });
 
 router.get("/product-delete/:id", (req, res, next) => {
-  console.log("toto");
   const SneakerId = req.params.id;
   Sneaker.findByIdAndDelete(SneakerId)
     .then((dbResult) => {
-      res.redirect("/");
+      res.redirect("/prod-manage");
     })
     .catch((error) => {
       next(error);
     });
+});
+
+router.get("/product-edit/:id", async (req, res, next) => {
+  try {
+    const sneakerId = req.params.id;
+    dbresult = await Sneaker.findById(sneakerId);
+    Sneaker.findByIdAndUpdate(sneakerId);
+    res.render("product_edit", { sneaker: dbresult });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/product-edit/:id", async (req, res, next) => {
+  try {
+    const sneakerId = req.params.id;
+    const updatedSneaker = await Sneaker.findByIdAndUpdate(sneakerId, req.body);
+    res.redirect("/prod-manage");
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;

c59fc2fe0e7adb47e4400a34d2ad98fa38f5d776
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-24 18:42:43 +0200

    cloudinary config done

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,5 +1,7 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
+const uploader = require("../config/cloudinary");
+
 
 const Sneaker = require("../models/Sneaker");
 

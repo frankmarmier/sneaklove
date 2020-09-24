9876fddf2ccd5f711d4584ea47d42fe2237dd75e
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-24 15:53:21 +0200

    modification models

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,30 @@
 const express = require("express");
 const router = new express.Router();
+const UserModel = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const salt = 25;
+
+router.post("/signup", async (req, res, next) => {
+    try {
+    
+    const newUser = req.body;
+
+    const foundUser = await UserModel.find({email: newUser.email})
+
+    if (foundUser) {
+        res.render("signup.hbs", {error : "Invalid credentials"})
+    } else {
+        const hashPassword = bcrypt.hashSync(newUser.password, salt);
+        newUser.password = hashPassword;
+        const user = await UserModel.create(newUser)
+    }
+
+    } catch(err){
+        next(err)
+    }
+})
+
 
 
 

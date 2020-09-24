144ab528e5e9401034344a8f408db7bc12ae2857
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-24 15:35:56 +0200

    fix model sneaker

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -6,12 +6,15 @@ const sneakerShema = new Schema({
   ref: String,
   size: Number,
   description: String,
-    price: Number,
-    category: { 
-        type: String,
-        enum: ['men', 'women', 'kids']
-    } ,
-    id_tags: [Schema.Types.ObjectId]
+  price: Number,
+  category: {
+    type: String,
+    enum: ["men", "women", "kids"],
+  },
+  id_tags: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Tag'
+  }],
 });
 
 const Sneaker = mongoose.model("Sneaker", sneakerShema);

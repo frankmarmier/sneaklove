2840cbaa29682af12ab0b2b48f6887cf2eda6c23
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-24 18:01:47 +0200

    C et R done

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,39 +1,53 @@
 const express = require("express"); // import express in this module
 const router = express.Router(); // create an app sub-module (router)
-const Sneaker = require('../models/Sneaker');
+const Sneaker = require("../models/Sneaker");
 const uploader = require("../config/cloudinary");
 
-router.get('/', (req, res, next) => {
-    res.render('products_manage');
-});
-
-router.get('/create', (req, res, next) => {
-    res.render('products_add');
+router.get("/create", (req, res, next) => {
+  res.render("products_add");
 });
 
 // C
 router.post(
-    '/prod-add',
-    uploader.single("image"), // Middleware function that allows you to read and upload to cloudinary
-    // The uploaded file can be found at req.file
-    async (req, res, next) => {
-      // DO something
-  
-      const newSneaker = req.body;
-      
-      if (req.file) {
-        newSneaker.image = req.file.path;
-      }
-      
-      try {
-        await Sneaker.create(newSneaker);
-        res.redirect('/dashboard/create');
-      } catch (error) {
-        next(error); // Sends us to the error handler middleware in app.js if an error occurs
-      }
-      //
-    }
-  );
+  "/prod-add",
+  uploader.single("image"), // Middleware function that allows you to read and upload to cloudinary
+  // The uploaded file can be found at req.file
+  async (req, res, next) => {
+    // DO something
 
+    const newSneaker = req.body;
+
+    if (req.file) {
+      newSneaker.image = req.file.path;
+    }
+
+    try {
+      await Sneaker.create(newSneaker);
+      res.redirect("/dashboard/");
+    } catch (error) {
+      next(error); // Sends us to the error handler middleware in app.js if an error occurs
+    }
+    //
+  }
+);
+
+// R
+router.get("/", async (req, res) => {
+  res.render("products_manage", { sneakers: await Sneaker.find() });
+});
+
+// U
+
+// D
+router.get("/product-delete/:id", async (req, res, next) => {
+  try {
+    console.log(req.params);
+    await Sneaker.findByIdAndRemove(req.params.id);
+    res.redirect("/");
+  }
+  catch(err) {
+      next(err)
+  }
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const Sneaker = require("../models/Sneaker");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/sneakers/:cat", (req, res) => {
-  res.send("plop");
+router.get('/sneakers/collection', async (req, res) => {
+  res.render('products', { sneakers : await Sneaker.find() });
 });
 
-router.get("/one-product/:id", (req, res) => {
-  res.send("baz");
+router.get("/sneakers/:cat", async (req, res) => {
+  res.render('products', { sneakers : await Sneaker.find({ category: req.params.cat }) });
+});
+
+router.get("/one-product/:id", async (req, res) => {
+  const sneaker = await Sneaker.find({ _id: req.params.id })
+  res.render('one_product', { sneaker : sneaker[0] });
 });
 
 router.get("/signup", (req, res) => {
@@ -20,6 +26,7 @@ router.get("/signup", (req, res) => {
 router.get("/signin", (req, res) => {
   res.send("");
 });
+
 
 
 module.exports = router;

16f197b485cdfeaab3821b963bc93744addaf988
pheno <nicolas.enault@gmail.com>
2020-09-25 10:04:29 +0200

    login done

--- routes/auth.js
+++ routes/auth.js
@@ -10,36 +10,25 @@ router.get("/signin", async (req, res, next) => {
 });
 
 router.post("/signin", async (req, res, next) => {
-  // DO something
-  //   res.render("auth/signin.hbs");
   const { email, password } = req.body;
   const foundUser = await User.findOne({ email: email });
   console.log(foundUser);
   if (!foundUser) {
-    //   Display an error message telling the user that either the password
-    // or the email is wrong
-    //   res.redirect("/signup")
     req.flash("error", "Invalid credentials");
     res.redirect("/signin");
-    // res.render("auth/signin.hbs", { error: "Invalid credentials" });
   } else {
     const isSamePassword = bcrypt.compareSync(password, foundUser.password);
     if (!isSamePassword) {
-      // Display an error message telling the user that either the password
-      // or the email is wrong
       req.flash("error", "Invalid credentials");
       res.redirect("/signin");
-      // res.render("auth/signin.hbs", { error: "Invalid credentials" });
     } else {
-      //
-      // Authenticate the user...
       const userDocument = { ...foundUser };
       console.log(userDocument);
       const userObject = foundUser.toObject();
-      delete userObject.password; // remove password before saving user in session
-      // console.log(req.session, "before defining current user");
-      req.session.currentUser = userObject; // Stores the user in the session
-      // console.log(req.session, "AFTER defining current user");
+      delete userObject.password;
+
+      req.session.currentUser = userObject;
+
       req.flash("success", "Successfully logged in...");
       res.redirect("/");
     }
@@ -67,14 +56,11 @@ router.post("/signup", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-  //   res.render("auth/signup.hbs");
 });
 
 router.get("/logout", async (req, res, next) => {
   console.log(req.session.currentUser);
   req.session.destroy(function (err) {
-    // cannot access session here
-    // console.log(req.session.currentUser);
     res.redirect("/signin");
   });
 });

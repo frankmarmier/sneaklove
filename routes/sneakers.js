a57d43838085e130ee4dc679b337db054045cb6c
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-25 10:27:20 +0200

    add tags to collections

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -2,22 +2,25 @@ const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
 
 const Sneaker = require("../models/Sneaker");
+const Tag = require("../models/Tag");
 
 router.get("/collection", async (req, res, next) => {
-    try {
-      const sneakers = await Sneaker.find();
-      const category = "whole"
-      res.render("products", { sneakers, category });
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const sneakers = await Sneaker.find();
+    const tags = await Tag.find();
+    const category = "whole";
+    res.render("products", { sneakers, category, tags });
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get("/men", async (req, res, next) => {
   try {
     const sneakers = await Sneaker.find({ category: "men" });
-    const category = 'men'
-    res.render("products", { sneakers, category });
+    const tags = await Tag.find();
+    const category = "men";
+    res.render("products", { sneakers, category, tags });
   } catch (err) {
     next(err);
   }
@@ -26,8 +29,9 @@ router.get("/men", async (req, res, next) => {
 router.get("/women", async (req, res, next) => {
   try {
     const sneakers = await Sneaker.find({ category: "women" });
-    const category = 'women'
-    res.render("products", { sneakers, category });
+    const tags = await Tag.find();
+    const category = "women";
+    res.render("products", { sneakers, category, tags });
   } catch (err) {
     next(err);
   }
@@ -36,8 +40,9 @@ router.get("/women", async (req, res, next) => {
 router.get("/kids", async (req, res, next) => {
   try {
     const sneakers = await Sneaker.find({ category: "kids" });
-    const category = 'kids'
-    res.render("products", { sneakers, category });
+    const tags = await Tag.find();
+    const category = "kids";
+    res.render("products", { sneakers, category, tags });
   } catch (err) {
     next(err);
   }

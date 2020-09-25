31764f74285c2f59e8d3015a8d827f56d86e8f23
Ailie McCorkindale <ailiem134@hotmail.com>
2020-09-25 12:21:30 +0200

    logout route added

--- routes/auth.js
+++ routes/auth.js
@@ -80,6 +80,13 @@ router.get("/signup", (req, res) => {
       }
     }
   });
+
+  router.get("/logout", (req, res, next) => {
+    req.session.destroy( function(error) {
+      res.redirect("/signin");
+    });
+   
+  })
   
 
 

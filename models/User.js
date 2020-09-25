dea9767169523ab09b607b96cf1bdad47cd1dc53
AyloSrd <aylo.srd@gmail.com>
2020-09-25 10:16:13 +0200

    trying to figure out flash

--- models/User.js
+++ models/User.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema ({
 	name: String,
 	lastname: String,
-	email: String,
-	password: String
+	email: { type: String, unique: true },
+	password: String,
+	role: { type:String, enum: ['admin', 'user'], default: 'user' }
 })
 
 const User = mongoose.model('User', UserSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-/*const User = require("../models/User");
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-const salt = 10;*/
+const salt = 10;
 
 router.get("/signin", async (req, res, next) => {
   res.render("signin");
@@ -16,30 +16,31 @@ router.get("/signup", async (req, res, next) => {
 router.post("/signup", async (req, res, next) => {
   try {
     const newUser = req.body;
-
+    console.log('1st', req.body)
     const foundUser = await User.findOne({ email: newUser.email });
-
+    console.log('2nd', foundUser)
     if (foundUser) {
-      res.render("auth/signup.hbs", { error: "Email already taken" });
+      req.flash(["error"], "Email already taken");
+      res.render("signup");
     } else {
       const hashedPassword = bcrypt.hashSync(newUser.password, salt);
       newUser.password = hashedPassword;
       const user = await User.create(newUser);
+      console.log('3rd', user)
       res.redirect("/auth/signin");
     }
   } catch (error) {
     next(error);
   }
-  //   res.render("auth/signup.hbs");
 });
 
 
-/*
+
 router.post("/signin", async (req, res, next) => {
   // DO something
   //   res.render("auth/signin.hbs");
   const { email, password } = req.body;
-  const foundUser = await User.findOne({ email: email });
+  const foundUser = await User.findOne({ email });
   console.log(foundUser);
   if (!foundUser) {
     //   Display an error message telling the user that either the password
@@ -63,28 +64,10 @@ router.post("/signin", async (req, res, next) => {
       delete userObject.password; // remove password before saving user in session
       req.session.currentUser = userObject; // Stores the user in the session
       req.flash("success", "Successfully logged in...");
-      res.redirect("/");
+      res.redirect("/sneakers/collection");
     }
   }
 });
-
-
-
-<<<<<<< HEAD
-=======
-    if (foundUser) {
-      res.render("auth/signup.hbs", { error: "Email already taken" });
-    } else {
-      const hashedPassword = bcrypt.hashSync(newUser.password, salt);
-      newUser.password = hashedPassword;
-      const user = await User.create(newUser);
-      res.redirect("/auth/signin");
-    }
-  } catch (error) {
-    next(error);
-  }
-});
->>>>>>> 2f27c2932876fa1de63ec5759c9d864c023240d5
 
 router.get("/logout", async (req, res, next) => {
   console.log(req.session.currentUser);
@@ -92,5 +75,5 @@ router.get("/logout", async (req, res, next) => {
     res.redirect("/auth/signin");
   });
 });
-*/
+
 module.exports = router;
